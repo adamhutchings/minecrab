6acdba4ac279e571cf4d00433086310edcbcede8
Brandon Qi <71684640+Sophon96@users.noreply.github.com>
2026-05-23 12:21:11 -0700

    Render Cube (#1)
    * chore: cargo init
    
    * wip: render unit cube
    
    * fix: properly hide cursor, disable vsync
    
    * feat: switch to sola-raylib and improve mvp

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use raylib::prelude::*;
+
+const WINDOW_WIDTH: i32 = 1280;
+const WINDOW_HEIGHT: i32 = 720;
+
+fn main() {
+    let (mut rl, thread) = raylib::init()
+        .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        .title("Hello, world!")
+        .vsync()
+        .highdpi()
+        .build();
+
+    let mut camera = Camera3D::perspective(
+        Vector3::new(3.0, 3.0, 3.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        45.0,
+    );
+
+    let mut first_click = false;
+
+    while !rl.window_should_close() {
+        // require a click on the window before updating camera so the camera
+        // doesn't fly away when the cursor enters the window at first
+        if !first_click {
+            if rl.is_mouse_button_down(MouseButton::MOUSE_BUTTON_LEFT) {
+                first_click = true;
+                rl.disable_cursor();
+            }
+        } else {
+            rl.update_camera(&mut camera, CameraMode::CAMERA_FIRST_PERSON);
+        }
+
+        rl.draw(&thread, |mut d| {
+            d.clear_background(Color::LIGHTBLUE);
+
+            if !first_click {
+                d.draw_text("WIP: Click to start updating camera", 20, 20, 16, Color::DARKGREEN);
+            }
+
+            d.draw_mode3D(camera, |mut d2, _camera| {
+                d2.draw_cube(Vector3::new(0.0, 0.0, 0.0), 1.0, 1.0, 1.0, Color::DARKGREEN);
+            });
+        });
+    }
+}
