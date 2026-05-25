c5571e10febe940a1cd3c5b03fe227bae1ac216e
gshutchings <questaka@gmail.com>
2026-05-25 12:19:09 -0700

    Make fixed tick rate

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use world::generation::World;
 const WINDOW_WIDTH: i32 = 1280;
 const WINDOW_HEIGHT: i32 = 720;
 
+const TICK_LENGTH: f32 = 0.025; // 40 ticks per second
+
 // Generate one chunk every [FRAMES_PER_CHUNK] frames so world generation isn't
 // exceedingly laggy at the beginning.
 const FRAMES_PER_CHUNK: i32 = 5;
@@ -27,6 +29,8 @@ fn main() {
 
     let mut first_click = false;
     let mut debug_display = false; // toggle
+
+    let mut update_camera_in = 0_f32; // time until we run update_camera()
 
     let texture: ffi::Texture = unsafe {
         let mut t = rl.load_texture(&thread, "assets/full-textures.png").unwrap();
@@ -51,8 +55,12 @@ fn main() {
                 rl.disable_cursor();
             }
         } else {
+            update_camera_in -= rl.get_frame_time();
             // rl.update_camera(&mut camera, CameraMode::CAMERA_FIRST_PERSON);
-            update_camera(&mut player, &mut rl);
+            while update_camera_in < 0_f32 { // in case a frame takes longer than a tick, update multiple times
+                update_camera(&mut player, &mut rl);
+                update_camera_in += TICK_LENGTH;
+            }
         }
         if rl.is_key_pressed(KeyboardKey::KEY_BACKSLASH) && first_click { // toggle debug menu
             debug_display = !debug_display;
